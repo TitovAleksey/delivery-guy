65e36480942e4d9cbd739bf0e57eb24428bdf8e0
Titov Aleksey <titov_al92@bk.ru>
2022-10-21 22:25:45 +0300

    finishes the algorithm of the widgets collision detection

--- src/main/kotlin/com/funsoftware/game/deliveryguy/leveleditor/UICanvas.kt
+++ src/main/kotlin/com/funsoftware/game/deliveryguy/leveleditor/UICanvas.kt
@@ -1,5 +1,6 @@
 package com.funsoftware.game.deliveryguy.leveleditor
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup
 import com.badlogic.gdx.scenes.scene2d.utils.Layout
@@ -15,12 +16,35 @@ class UICanvas : WidgetGroup() {
     }
 
     override fun layout() {
-        val sortedXVertexes = children.map { it as AutoMovableWidget }
+        var intersectionCount = 0
+        val activeWidgets = HashSet<AutoMovableWidget>()
+        children.map { it as AutoMovableWidget }
             .flatMap { it.getXVertexes().toList() }
             .sortedWith { v1, v2 ->
                 val result = compareValues(v1.coordinate, v2.coordinate)
                 if (result == 0) compareValues(v1.isFirst, v2.isFirst) else result
             }
-            .toList()
+            .forEach { vertex ->
+                if (vertex.isFirst) {
+                    activeWidgets.forEach { widget ->
+                        if (isThereIntersectionByYScale(vertex.widget, widget)) {
+                            intersectionCount++
+                        }
+                    }
+                    activeWidgets.add(vertex.widget)
+                } else {
+                    activeWidgets.remove(vertex.widget)
+                }
+            }
+        Gdx.app.log("some tag", "Intersections count: $intersectionCount")
+    }
+
+    private fun isThereIntersectionByYScale(widget1: AutoMovableWidget, widget2: AutoMovableWidget): Boolean {
+        val widget1Vertexes = widget1.getYVertexes()
+        val widget2Vertexes = widget2.getYVertexes()
+        return widget1Vertexes.first.coordinate in widget2Vertexes.first.coordinate..widget2Vertexes.second.coordinate
+                || widget1Vertexes.second.coordinate in widget2Vertexes.first.coordinate..widget2Vertexes.second.coordinate
+                || (widget1Vertexes.second.coordinate >= widget2Vertexes.second.coordinate
+                && widget1Vertexes.first.coordinate <= widget2Vertexes.first.coordinate)
     }
 }
